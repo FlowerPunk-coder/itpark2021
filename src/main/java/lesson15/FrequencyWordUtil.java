68406ab0d180ae4d2ef77d811603f4101d68d1a2
Max <maxim.ketikov@gmail.com>
2021-12-25 22:07:44 +0300

    lesson15.StreamApi

---
+++ src/main/java/lesson15/FrequencyWordUtil.java
@@ -0,0 +1,19 @@
+package lesson15;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class FrequencyWordUtil {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите строку: ");
+        String str = scanner.nextLine();
+        String[] words = str.split("[ ,.!/?]+");
+        Map<String, Integer> map  = Arrays.stream(words).collect(Collectors.toMap(Function.identity(), value -> 1, Integer::sum));
+        map.entrySet().stream().sorted((a, b) -> b.getValue() - a.getValue()).forEach(System.out::println);
+    }
+}
