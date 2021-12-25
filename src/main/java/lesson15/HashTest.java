3cdbd654c1aed26c391502f928e39b38fb0091f5
Max <maxim.ketikov@gmail.com>
2021-12-25 18:03:03 +0300

    lesson15.Разница между уникальным хешкодом и одинаковым

---
+++ src/main/java/lesson15/HashTest.java
@@ -0,0 +1,36 @@
+package lesson15;
+
+import lesson14.Caller;
+import lesson14.factory.AddressBookAtSet;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class HashTest {
+
+    public static void main(String[] args) {
+
+        long start = System.currentTimeMillis();
+        Set<Caller> callers = new HashSet<>();
+         for (int i = 0; i < 10000; i++) {
+             callers.add(new Caller());
+         }
+        Caller tempCaller = new Caller();
+        System.out.println(callers.contains(tempCaller));
+        long end = System.currentTimeMillis();
+        System.out.println(end - start + " mc, занял поиск с уникальными значениями хешкода");
+
+        start = System.currentTimeMillis();
+        Set<TestCaller> testCallerSet = new HashSet<>();
+        for (int i = 0; i < 10000; i++) {
+            testCallerSet.add(new TestCaller());
+        }
+        TestCaller testCaller = new TestCaller();
+        System.out.println(testCallerSet.contains(testCaller));
+        end = System.currentTimeMillis();
+        System.out.println(end - start + " mc, занял поиск с одинаковыми значениями хешкода");
+
+    }
+
+
+}
---
+++ src/main/java/lesson15/TestCaller.java
@@ -0,0 +1,71 @@
+package lesson15;
+
+import lesson13.creator.FemaleFullNameUtils;
+import lesson13.creator.MaleFullNameUtils;
+import lesson14.Caller;
+import lesson14.factory.PhoneNumberUtil;
+
+import java.util.*;
+
+public class TestCaller {
+
+    private final String fio;
+    private String phoneNumber;
+    private List<TestCaller> callerList;
+    private Set<TestCaller> callerSet;
+    private Map<Integer, TestCaller> callerMap;
+
+    public TestCaller() {
+        this.fio = new Random().nextBoolean() ? MaleFullNameUtils.getFio() : FemaleFullNameUtils.getFio();
+        this.phoneNumber = PhoneNumberUtil.getPhoneNumber();
+    }
+
+    public List<TestCaller> getCallerList() {
+        return callerList;
+    }
+
+    public Set<TestCaller> getCallerSet() {return callerSet;}
+
+    public Map<Integer, TestCaller> getCallerMap() {return callerMap;}
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestCaller testCaller = (TestCaller) o;
+        return Objects.equals(phoneNumber, testCaller.phoneNumber);
+    }
+
+    @Override
+    public int hashCode() {
+        return 31;
+    }
+
+    public String getFio() {
+        return fio;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String toString() {
+        return fio + " " + phoneNumber;
+    }
+
+    public void setCallerList(List<TestCaller> callerList) {
+        this.callerList = callerList;
+    }
+
+    public void setCallerSet(Set<TestCaller> callerSet) {
+        this.callerSet = callerSet;
+    }
+
+    public void setCallerMap(Map<Integer, TestCaller> callerMap) {
+        this.callerMap = callerMap;
+    }
+}
