7bcaf88c2cf41350cf9369241b76c58e78e18429
Max <maxim.ketikov@gmail.com>
2022-01-09 23:29:22 +0300

    lesson18.Из файла в файлы.

--- src/main/java/lesson18/FileReaderRunner.java
+++ src/main/java/lesson18/FileReaderRunner.java
@@ -1,28 +1,30 @@
 package lesson18;
 
-import java.io.BufferedReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class FileReaderRunner {
 
     public static void main(String[] args) {
 
-        new Thread(() -> {
-            try (BufferedReader bR = new BufferedReader(new InputStreamReader((FileReaderRunner.class.getResourceAsStream("/text.txt"))))) {
-                String path = "F:/test/text/text";
-                int i = 1;
-                while (bR.ready()) {
-                    FileWriter fW = new FileWriter(path + i + ".txt");
-                    fW.write(bR.readLine());
-                    fW.flush();
-                    fW.close();
-                    i++;
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
+        new Thread(() ->
+            FileReaderRunner.fileToFile(FileReaderRunner
+                    .class
+                    .getResourceAsStream("/text.txt"), "f:/test/text/text.txt")).start();
+    }
+
+    public static void fileToFile(InputStream in, String path) {
+        try (BufferedReader bR = new BufferedReader(new InputStreamReader((in)))) {
+            String[] fullPath = path.split("\\.");
+            int i = 1;
+            while (bR.ready()) {
+                FileWriter fW = new FileWriter(fullPath[0] + i + "." + fullPath[1]);
+                fW.write(bR.readLine());
+                fW.flush();
+                fW.close();
+                i++;
             }
-        }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
